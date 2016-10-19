e9d431b1a18036cd3e67dd510707231d03e50a37
larry <larrylv@newcreds.com>
2016-10-19 21:07:29 +0800

    update 添加过滤器

--- src/main/java/com/infogen/rpc/filter/InfoGen_Filter.java
+++ src/main/java/com/infogen/rpc/filter/InfoGen_Filter.java
@@ -1,5 +1,7 @@
 package com.infogen.rpc.filter;
 
+import java.util.Map;
+
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
@@ -10,5 +12,9 @@ import io.netty.handler.codec.http.FullHttpResponse;
  * @version 1.0
  */
 public abstract class InfoGen_Filter {
+	public abstract void init(Map<String,String> filterConfig);
+
+	public abstract void destroy();
+
 	public abstract Boolean doFilter(ChannelHandlerContext ctx, FullHttpRequest request, FullHttpResponse response);
 }
